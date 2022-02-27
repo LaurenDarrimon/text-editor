6c804c4dcb544f01da282d5288e887805575f060
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-26 20:05:44 -0800

    Implement asset and image cahing via page cache in workbox register route function

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -8,7 +8,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {event.prompt();
+butInstall.addEventListener('click', async (event) => {event.prompt();
     butInstall.setAttribute('disabled', true);
     butInstall.textContent = 'Installed!';
 });
