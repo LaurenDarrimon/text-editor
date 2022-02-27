3d9cdba9709412f0db6665e10ec713eb2cf94744
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-26 19:39:39 -0800

    add css loader to webpack configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { GenerateSW } = require("workbox-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 
 const { InjectManifest } = require("workbox-webpack-plugin");
@@ -22,10 +22,9 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Just Another Text Editor - J.A.T.E.",
+        title: "J.A.T.E.",
       }),
-
-      new GenerateSW(),
+      new MiniCssExtractPlugin(),
       new WebpackPwaManifest({
         name: "JATE",
         short_name: "JATE",
@@ -36,19 +35,22 @@ module.exports = () => {
         publicPath: "/",
         icons: [
           {
-            src: path.resolve("assets/images/logo.png"),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join("assets", "icons"),
           },
         ],
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+      })
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -56,7 +58,7 @@ module.exports = () => {
         },
         {
           test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
+          exclude: /(node_modules)/,
           use: {
             loader: "babel-loader",
             options: {
