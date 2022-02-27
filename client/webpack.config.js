aca150fa37781323c025e6faa04e112e2eda605e
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-26 20:28:07 -0800

    reconfigure webpack with babel to remove runtime errors

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 const { InjectManifest } = require("workbox-webpack-plugin");
@@ -25,6 +25,9 @@ module.exports = () => {
         title: "J.A.T.E.",
       }),
       new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+      }),
       new WebpackPwaManifest({
         name: "JATE",
         short_name: "JATE",
@@ -41,31 +44,28 @@ module.exports = () => {
           },
         ],
       }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-      })
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
           type: "asset/resource",
         },
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        },
+        // {
+        //   test: /\.m?js$/,
+        //   exclude: /(node_modules)/,
+        //   use: {
+        //     loader: "babel-loader",
+        //     options: {
+        //       presets: ["@babel/preset-env"],
+        //     },
+        //   },
+        // },
       ],
     },
   };
