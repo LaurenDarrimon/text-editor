c0a006adf43055a57619e0010369118a924fb26c
LaurenDarrimon <laurendarrimon@gmail.com>
2022-02-27 21:31:55 -0800

    configure event handling on install button functions

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,6 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     // TODO: Implement a click event handler on the `butInstall` element
     butInstall.addEventListener('click', async () => {
         event.prompt();
+        console.log("Install button clicked");
         butInstall.setAttribute('disabled', true);
         butInstall.textContent = 'Installed!';
     });
